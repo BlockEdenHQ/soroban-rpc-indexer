733ec596f2b9be4a62bfbace4ef8601955e6bd64
Tian Pan <puncsky@gmail.com>
2024-04-02 14:15:17 -0700

    update

--- cmd/soroban-rpc/internal/indexer/model/util/ts.go
+++ cmd/soroban-rpc/internal/indexer/model/util/ts.go
@@ -3,6 +3,6 @@ package util
 import "time"
 
 type Ts struct {
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `gorm:"type:datetime"`
+	UpdatedAt time.Time `gorm:"type:datetime"`
 }
